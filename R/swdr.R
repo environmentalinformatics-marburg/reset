d27b04fb4c7143011e6ec6806c4cd705b9a41487
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-03-29 14:21:19 +0200

    Revised documentation.

--- R/swdr.R
+++ R/swdr.R
@@ -1,5 +1,6 @@
 #' Shortwave downward radiation
 #'
+#' @description
 #' Shortwave downward radiation model proposed by Allen \emph{et al.} (2007; see
 #' References).
 #'
@@ -12,13 +13,13 @@
 #' \code{\link{atmosTrans}}.
 #'
 #' @references
-#' Allen, R.G., Tasumi, M. and R. Trezza (2007). Satellite-Based Energy Balance
-#' for Mapping Evapotranspiration with Internalized Calibration (METRIC) --
-#' Model. \emph{Journal of Irrigation and Drainage Engineering} 133(4), 395-406,
+#' Allen RG, Tasumi M, Trezza R (2007) Satellite-Based Energy Balance for
+#' Mapping Evapotranspiration with Internalized Calibration (METRIC) -- Model.
+#' \emph{Journal of Irrigation and Drainage Engineering} 133(4), 395-406,
 #' \url{http://dx.doi.org/10.1061/(ASCE)0733-9437(2007)133:4(395)}
 #' (accessed 2016-03-07).
 #'
-#' @seealso \code{\link{calcEarthSunDist}}.
+#' @seealso \code{\link{calcEarthSunDist}}, \code{\link{atmosTrans}}.
 #'
 #' @export swdr
 #' @name swdr
